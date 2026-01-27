5cc8ea383f83cf264d091d0e8aec736d29b36e9a
StudioWEngineers <studio.w.engineers@gmail.com>
2026-01-27 22:40:00 +0100

    clean-up conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,27 +1,17 @@
-import os
-import sys
-
-# -- Path setup ------------------------------------------------------------
-sys.path.insert(0, os.path.abspath("../src"))
-
-# -- Project information ---------------------------------------------------
+# -- Project information --
 project = "py-sofistik-utils"
 author = "StudioWEngineers"
 copyright = "2026, StudioWEngineers"
 
-# -- General configuration -------------------------------------------------
+# -- General configuration --
 extensions = [
-    "sphinx.ext.autodoc",           # core autodoc
+    "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
-    "sphinx.ext.intersphinx",       # links to Python, numpy, pandas
+    "sphinx.ext.intersphinx",
     "sphinx_copybutton",
     "sphinx.ext.napoleon"
 ]
 
-# Hide full module names in docs
-add_module_names = False
-
-# Autodoc options
 autodoc_default_options = {
     "members": True,
     "undoc-members": True,
@@ -29,24 +19,19 @@ autodoc_default_options = {
     "inherited-members": True,
 }
 
-# Default role for `text`
 default_role = "any"
 templates_path = ["_templates"]
 
-# Exclude patterns
-#exclude_patterns = [".build", "release.rst", "**/_internal/**", "**/tests/**"]
-
-# Primary language
 primary_domain = "py"
 highlight_language = "python"
 
-# Intersphinx mapping
+add_module_names = False
+
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
     "pandas": ("https://pandas.pydata.org/pandas-docs/stable/", None),
     "numpy": ("https://numpy.org/doc/stable/", None)
 }
 
-# -- Options for HTML output ----------------------------------------------
 html_theme = "furo"
 html_title = f"{project} documentation"
