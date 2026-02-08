f8f33cf2af87b68649c0559ae12965738c91f053
StudioWEngineers <studio.w.engineers@gmail.com>
2026-02-08 19:16:58 +0100

    update load

--- src/py_sofistik_utils/cdb_reader/_internal/cable_results.py
+++ src/py_sofistik_utils/cdb_reader/_internal/cable_results.py
@@ -2,10 +2,10 @@
 from ctypes import byref, c_int, sizeof
 
 # third party library imports
-from pandas import concat, DataFrame, Series
+from pandas import concat, DataFrame
 
 # local library specific imports
-from . group_lc_data import _GroupLCData
+from . group_data import _GroupData
 from . sofistik_dll import SofDll
 from . sofistik_classes import CCABL_RES
 
@@ -52,66 +52,87 @@ class _CableResults:
         self._data = self._data[0:0]
         self._loaded_lc.clear()
 
-    def load(self, load_case: int) -> None:
-        """Load the cable results for the given ``load_case`` number.
+    def load(self, load_cases: int | list[int]) -> None:
+        """Retrieve cable results for the given ``load_cases``. If a load case is not
+        found, a warning is raised only if ``echo_level > 0``.
 
         Parameters
         ----------
-        ``load_case``: int
-
-        Raises
-        ------
-        RuntimeError
-            If the given ``load_case`` is not found.
+        load_cases : int | list[int]
+            load case numbers
         """
-        if self._dll.key_exist(162, load_case):
-            cable_res = CCABL_RES()
+        if isinstance(load_cases, int):
+            load_cases = [load_cases]
+
+        # load data
+        temp_list: list[dict[str, float | int | str]] = []
+        for load_case in load_cases:
+            if self._dll.key_exist(162, load_case):
+                self.clear(load_case)
+                temp_list.extend(self._load(load_case))
+
+        # assigning groups
+        group_data = _GroupData(self._dll)
+        group_data.load()
+
+        temp_df = DataFrame(temp_list).sort_values("ELEM_ID", kind="mergesort")
+        elem_ids = temp_df["ELEM_ID"]
+
+        for grp, grp_range in group_data.iterator_cable():
+            if grp_range.stop == 0:
+                continue
+
+            left = elem_ids.searchsorted(grp_range.start, side="left")
+            right = elem_ids.searchsorted(grp_range.stop - 1, side="right")
+            temp_df.loc[temp_df.index[left:right], "GROUP"] = grp
+
+        # set indices for fast lookup
+        temp_df = temp_df.set_index(["ELEM_ID", "LOAD_CASE"], drop=False)
+
+        # merge data
+        if self._data.empty:
+            self._data = temp_df
+        else:
+            self._data = concat([self._data, temp_df])
+        self._loaded_lc.update(load_cases)
+
+    def _load(self, load_case: int) -> list[dict[str, float | int | str]]:
+        """Retrieve key ``162/load_case`` using SOFiSTiK dll.
+        """
+        cable_res = CCABL_RES()
+        record_length = c_int(sizeof(cable_res))
+        return_value = c_int(0)
+
+        data: list[dict[str, float | int | str]] = []
+        first_call = True
+        while return_value.value < 2:
+            return_value.value = self._dll.get(
+                1,
+                162,
+                load_case,
+                byref(cable_res),
+                byref(record_length),
+                0 if first_call else 1
+            )
+
             record_length = c_int(sizeof(cable_res))
-            return_value = c_int(0)
+            first_call = False
+            if return_value.value >= 2:
+                break
 
-            self.clear(load_case)
-
-            temp_container = []
-            count = 0
-            while return_value.value < 2:
-                return_value.value = self._dll.get(
-                    1,
-                    162,
-                    load_case,
-                    byref(cable_res),
-                    byref(record_length),
-                    0 if count == 0 else 1
+            if cable_res.m_nr > 0:
+                data.append(
+                    {
+                        "LOAD_CASE": load_case,
+                        "GROUP": 0,
+                        "ELEM_ID": cable_res.m_nr,
+                        "AXIAL_FORCE": cable_res.m_n,
+                        "AVG_AXIAL_FORCE": cable_res.m_n_m,
+                        "AXIAL_DISPLACEMENT": cable_res.m_v,
+                        "RELAXED_LENGTH": cable_res.m_l0,
+                        "TOTAL_STRAIN": cable_res.m_eps0,
+                        "EFFECTIVE_STIFFNESS": cable_res.m_effs,
+                    }
                 )
 
-                if return_value.value >= 2:
-                    break
-
-                if cable_res.m_nr > 0:
-                    temp_container.append({"LOAD_CASE": load_case,
-                                           "GROUP": 0,
-                                           "ELEM_ID": cable_res.m_nr,
-                                           "AXIAL_FORCE": cable_res.m_n,
-                                           "AVG_AXIAL_FORCE": cable_res.m_n_m,
-                                           "AXIAL_DISPLACEMENT": cable_res.m_v,
-                                           "RELAXED_LENGTH": cable_res.m_l0,
-                                           "TOTAL_STRAIN": cable_res.m_eps0,
-                                           "EFFECTIVE_STIFFNESS": cable_res.m_effs,
-                                           })
-
-                record_length = c_int(sizeof(cable_res))
-                count += 1
-
-            data = DataFrame(temp_container)
-
-            # assigning groups
-            group_lc_data = _GroupLCData(self._dll)
-            group_lc_data.load(load_case)
-
-            for grp, cable_range in group_lc_data.iterator_cable(load_case):
-                data.loc[data.ELEM_ID.isin(cable_range), "GROUP"] = grp
-
-            if self._data.empty:
-                self._data = data
-            else:
-                self._data = concat([self._data, data], ignore_index=True)
-            self._loaded_lc.add(load_case)
+        return data
