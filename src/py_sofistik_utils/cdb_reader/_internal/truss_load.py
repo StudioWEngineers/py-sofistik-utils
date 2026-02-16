de95eafb331f75f20d563a3f9ba45c601365711d
StudioWEngineers <studio.w.engineers@gmail.com>
2026-02-16 08:06:29 +0100

    update load

--- src/py_sofistik_utils/cdb_reader/_internal/truss_load.py
+++ src/py_sofistik_utils/cdb_reader/_internal/truss_load.py
@@ -1,6 +1,5 @@
 # standard library imports
 from ctypes import byref, c_int, sizeof
-from typing import Any
 
 # third party library imports
 from pandas import concat, DataFrame
@@ -17,6 +16,22 @@ class _TrussLoad:
     * store these data in a convenient format;
     * provide access to these data.
     """
+    _LOAD_TYPE_MAP = {
+        10: "PG",
+        11: "PXX",
+        12: "PYY",
+        13: "PZZ",
+        30: "EX",
+        31: "WX",
+        60: "DT",
+        61: "DT",
+        70: "VX",
+        80: "VX",
+        111: "PXP",
+        212: "PYP",
+        313: "PZP"
+    }
+
     def __init__(self, dll: SofDll) -> None:
         self._data = DataFrame(
             columns=[
@@ -112,7 +127,9 @@ class _TrussLoad:
             If the requested load is not found and ``default`` is None.
         """
         try:
-            return self._data.at[(element_id, load_case, load_type), point]  # type: ignore
+            return self._data.at[
+                (element_id, load_case, load_type), point
+            ]  # type: ignore
         except (KeyError, ValueError) as e:
             if default is not None:
                 return default
@@ -122,109 +139,104 @@ class _TrussLoad:
             ) from e
 
     def load(self, load_cases: int | list[int]) -> None:
-        """Load cable element loads for the given the ``load_cases``.
-
-        If a load case is not found, a warning is raised only if ``echo_level`` is ``> 0``.
+        """Retrieve truss load data for the given ``load_cases``. If a load
+        case is not found, a warning is raised only if ``echo_level > 0``.
 
         Parameters
         ----------
-        ``load_cases``: int | list[int], load case numbers
+        load_cases : int | list[int]
+            load case numbers
+
+        Notes
+        -----
+        Wind and snow loads are not implemented and will raise a runtime error
+        if they are present in the requested load case.
         """
         if isinstance(load_cases, int):
             load_cases = [load_cases]
+        else:
+            load_cases = list(set(load_cases))  # remove duplicated entries
 
+        # load data
+        temp_list: list[dict[str, float | int | str]] = []
         for load_case in load_cases:
             if self._dll.key_exist(151, load_case):
                 self.clear(load_case)
-
-                # load data
-                data = DataFrame(self._load(load_case))
-
-                # merge data
-                if self._data.empty:
-                    self._data = data
-                else:
-                    self._data = concat([self._data, data], ignore_index=True)
-                self._loaded_lc.add(load_case)
-
-            else:
-                continue
+                temp_list.extend(self._load(load_case))
 
         # assigning groups
         group_data = _GroupData(self._dll)
         group_data.load()
 
-        for grp, cable_range in group_data.iterator_truss():
-            self._data.loc[self._data.ELEM_ID.isin(cable_range), "GROUP"] = grp
+        temp_df = DataFrame(temp_list).sort_values("ELEM_ID", kind="mergesort")
+        elem_ids = temp_df["ELEM_ID"]
+
+        for grp, grp_range in group_data.iterator_truss():
+            if grp_range.stop == 0:
+                continue
+
+            left = elem_ids.searchsorted(grp_range.start, side="left")
+            right = elem_ids.searchsorted(grp_range.stop - 1, side="right")
+            temp_df.loc[temp_df.index[left:right], "GROUP"] = grp
+
+        # set indices for fast lookup
+        temp_df = temp_df.set_index(
+            ["ELEM_ID", "LOAD_CASE", "TYPE"],
+            drop=False
+        )
+
+        # merge data
+        if self._data.empty:
+            self._data = temp_df
+        else:
+            self._data = concat([self._data, temp_df])
+        self._loaded_lc.update(load_cases)
 
     def set_echo_level(self, echo_level: int) -> None:
         """Set the echo level.
         """
         self._echo_level = echo_level
 
-    def _load(self, load_case: int) -> list[dict[str, Any]]:
+    def _load(self, load_case: int) -> list[dict[str, float | int | str]]:
         """
         """
-        cabl = CTRUS_LOA()
-        record_length = c_int(sizeof(cabl))
+        trus = CTRUS_LOA()
+        record_length = c_int(sizeof(trus))
         return_value = c_int(0)
 
-        data: list[dict[str, Any]] = []
-        count = 0
+        data: list[dict[str, float | int | str]] = []
+        first_call = True
         while return_value.value < 2:
             return_value.value = self._dll.get(
                 1,
                 151,
                 load_case,
-                byref(cabl),
+                byref(trus),
                 byref(record_length),
-                0 if count == 0 else 1
+                0 if first_call else 1
             )
 
-            record_length = c_int(sizeof(cabl))
-            count += 1
-
+            record_length = c_int(sizeof(trus))
+            first_call = False
             if return_value.value >= 2:
                 break
 
-            match cabl.m_typ:
-                case 10:
-                    type_ = "PG"
-                case 11:
-                    type_ = "PXX"
-                case 12:
-                    type_ = "PYY"
-                case 13:
-                    type_ = "PZZ"
-                case 30:
-                    type_ = "EX"
-                case 31:
-                    type_ = "WX"
-                case 60:
-                    type_ = "T"
-                case 61:
-                    type_ = "DT"
-                case 70:
-                    type_ = "VX"
-                case 80:
-                    type_ = "VX"
-                case 111:
-                    type_ = "PXP"
-                case 212:
-                    type_ = "PYP"
-                case 313:
-                    type_ = "PZP"
-                case _:
-                    raise RuntimeError(f"Unknown type: {cabl.m_typ}!")
+            try:
+                type_ = _TrussLoad._LOAD_TYPE_MAP[trus.m_typ]
+            except KeyError as e:
+                raise RuntimeError(
+                    f"Unknown truss load type {trus.m_typ} for element "
+                    f"{trus.m_nr}!"
+                ) from e
 
             data.append(
                 {
                     "LOAD_CASE":    load_case,
                     "GROUP":        0,
-                    "ELEM_ID":      cabl.m_nr,
+                    "ELEM_ID":      trus.m_nr,
                     "TYPE":         type_,
-                    "PA":           cabl.m_pa,
-                    "PE":           cabl.m_pe,
+                    "PA":           trus.m_pa,
+                    "PE":           trus.m_pe,
                 }
             )
 
