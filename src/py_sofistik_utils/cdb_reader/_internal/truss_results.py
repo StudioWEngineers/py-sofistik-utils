43d1723ee4b0b5646190b5528e54234354a4bbd7
StudioWEngineers <studio.w.engineers@gmail.com>
2026-02-16 15:37:24 +0100

    add data method

--- src/py_sofistik_utils/cdb_reader/_internal/truss_results.py
+++ src/py_sofistik_utils/cdb_reader/_internal/truss_results.py
@@ -49,6 +49,20 @@ class _TrussResult:
         self._data = self._data[0:0]
         self._loaded_lc.clear()
 
+    def data(self, deep: bool = True) -> DataFrame:
+        """Return the :class:`pandas.DataFrame` containing the loaded keys
+        ``152/LC``.
+
+        Parameters
+        ----------
+        deep : bool, default True
+            When ``deep=True``, a new object will be created with a copy of the
+            calling object's data and indices. Modifications to the data or
+            indices of the copy will not be reflected in the original object
+            (refer to :meth:`pandas.DataFrame.copy` documentation for details).
+        """
+        return self._data.copy(deep=deep)
+
     def get_element_force(self, element_number: int, load_case: int) -> float:
         """Return the cable connectivity for the given ``element_number``.
 
