ed77cfe67de3ae2f7e42cfa812aab865c202f46c
StudioWEngineers <studio.w.engineers@gmail.com>
2026-02-17 22:40:48 +0100

    update docs

--- src/py_sofistik_utils/cdb_reader/_internal/cable.py
+++ src/py_sofistik_utils/cdb_reader/_internal/cable.py
@@ -18,6 +18,10 @@ class _Cable:
     provides a structured entry point for reading, manipulating and evaluating
     cable definitions, applied loads, and analysis results.
     """
+    data: _CableData
+    load: _CableLoad
+    result: _CableResult
+
     def __init__(self, dll: SofDll) -> None:
         self.data = _CableData(dll)
         self.load = _CableLoad(dll)
