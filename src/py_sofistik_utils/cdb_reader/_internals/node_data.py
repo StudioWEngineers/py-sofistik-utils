49d77e55dd380b8c95ce0674257bf96065d53d0d
StudioWEngineers <studio.w.engineers@gmail.com>
2026-03-31 20:16:46 +0200

    update node_data + tests

--- src/py_sofistik_utils/cdb_reader/_internals/node_data.py
+++ src/py_sofistik_utils/cdb_reader/_internals/node_data.py
@@ -1,6 +1,5 @@
 # standard library imports
 from ctypes import byref, c_int, sizeof
-from typing import Any
 
 # third party library imports
 from pandas import DataFrame
@@ -12,91 +11,123 @@ from . sofistik_utilities import decode_nodal_boundary_condition
 
 
 class _NodeData:
-    """
-    The ``_NodeData`` class provides methods and data structure to:
+    """This class provides methods and a data structure to:
 
-    * access and load the keys ``20/00`` of the CDB file;
-    * store these data in a convenient format;
-    * provide access to these data.
+        * access keys ``2/00`` of the CDB file;
+        * store the retrieved data in a convenient format;
+        * provide access to the data after the CDB is closed.
 
-    Node data are stored in a :class:`pandas.DataFrame` with the following columns:
+        The underlying data structure is a :class:`pandas.DataFrame` with the
+        following columns:
 
-    * ``ID``: the node number
-    * ``INT_ID``: the node internal number
-    * ``X0``: the X coordinate of the node
-    * ``Y0``: the Y coordinate of the node
-    * ``Z0``: the Z coordinate of the node
-    * ``KFIX``: string defining the boundary conditions defined for the node
-    * ``IS_USED``: `bool`, `True` if the node is connected to already engaged nodes
+        * ``ID`` node number
+        * ``X0`` X-coordinate
+        * ``Y0``: Y-coordinate
+        * ``Z0``: Z-coordinate
+        * ``KFIX``: lateral stiffness
+        * ``IS_:USED``: rotational stiffness
+
+        The ``DataFrame`` uses a MultiIndex with level ``ID`` to enable fast
+        lookups via the `get` method. The index column is not dropped from the
+        ``DataFrame``.
+
+        .. note::
+
+            Not all available quantities are retrieved and stored. In
+            particular:
+
+            * internal node id ("INR)
+            * additional bit code ("NCOD")
+
+            are currently not included.
+
+            This is a deliberate design choice and may be changed in the future
+            without breaking the existing API.
     """
     def __init__(self, dll: SofDll) -> None:
-        """The initializer of the ``_NodeData`` class.
-        """
         self._data = DataFrame(
-            columns = ["ID", "INT_ID", "X0", "Y0", "Z0", "KFIX", "IS_USED"]
+            columns=[
+                "ID",
+                "X0",
+                "Y0",
+                "Z0",
+                "KFIX",
+                "IS_USED"
+            ]
         )
         self._dll = dll
         self._is_loaded = False
 
     def clear(self) -> None:
-        """Clear the nodal information.
+        """Clear all the loaded data.
         """
         if self._is_loaded:
             self._data = self._data[0:0]
             self._is_loaded = False
 
-    def drop_not_used_nodes(self) -> None:
-        """Remove all the not used nodes.
+    def data(self, deep: bool = True) -> DataFrame:
+        """Return the :class:`pandas.DataFrame` containing the loaded key
+        ``20/00``.
+
+        Parameters
+        ----------
+        deep : bool, default True
+            When ``deep=True``, a new object will be created with a copy of the
+            calling object's data and indices. Modifications to the data or
+            indices of the copy will not be reflected in the original object
+            (refer to :meth:`pandas.DataFrame.copy` documentation for details).
+        """
+        return self._data.copy(deep=deep)
+
+    def drop_unused_nodes(self) -> None:
+        """Remove all the unused nodes.
         """
         self._data = self._data.loc[self._data.IS_USED, :]
 
-    def get_all_coordinates(self) -> Any:
-        """Return all the nodal coordinates.
-        """
-        return self._data[["ID", "X0", "Y0", "Z0"]].copy(deep=True)
-
-    def get_boundary_condition(self, node_number: int) -> str:
-        """Return the nodal boundary conditions for the given ``node_number``.
-
-        Parameters
-        ----------
-        ``node_number``: int
-
-        Raises
-        ------
-        RuntimeError
-            If the given ``node_number`` is not found.
-        """
-        mask = self._data["ID"] == node_number
-
-        if mask.eq(False).all():
-            raise RuntimeError(f"Node number {node_number} not found!")
-
-        return self._data.KFIX[mask].item()  #type: ignore
-
-    def get_coordinates(self, node_number: int) -> Any:
-        """Return the nodal coordinates for the given ``node_number``.
+    def get(
+            self,
+            node_id: int,
+            quantity: str,
+            default: bool | float | None = None
+    ) -> float | int | bool:
+        """Retrieve the requested nodal quantity.
 
         Parameters
         ----------
-        ``node_number``: int
+        node_id : int
+            Node number
+        quantity : str
+            Quantity to retrieve. Must be one of:
+
+            - ``"X0"``
+            - ``"Y0"``
+            - ``"Z0"``
+            - ``"KFIX"``
+            - ``"IS_USED"``
+
+        default : float or None, default None
+            Value to return if the requested quantity is not found
+
+        Returns
+        -------
+        value : float
+            The requested quantity if found. Otherwise, returns ``default``
+            when it is not None.
 
         Raises
         ------
-        RuntimeError
-            If the given ``node_number`` is not found.
+        LookupError
+            If the requested quantity is not found and ``default`` is None.
         """
-        mask = self._data["ID"] == node_number
-
-        if mask.eq(False).all():
-            raise RuntimeError(f"Node number {node_number} not found!")
-
-        return self._data[["X0", "Y0", "Z0"]][mask].copy(deep=True)
-
-    def get_number_of_nodes(self) -> int:
-        """Return the number of nodes.
-        """
-        return self._data.ID.size
+        try:
+            return self._data.at[node_id, quantity]  # type: ignore
+        except (KeyError, ValueError) as e:
+            if default is not None:
+                return default
+            raise LookupError(
+                f"Node data entry not found for node id {node_id}, and "
+                f"quantity {quantity}!"
+            ) from e
 
     def is_loaded(self) -> bool:
         """Return ``True`` if the nodal data have been loaded from the cdb.
@@ -104,7 +135,8 @@ class _NodeData:
         return self._is_loaded
 
     def load(self) -> None:
-        """Load nodal data for all the nodes.
+        """Retrieve all nodal data. If the key does not exist or it is empty,
+        a warning is raised only if ``echo_level > 0``.
         """
         if self._dll.key_exist(20, 0):
             node = CNODE()
@@ -113,8 +145,8 @@ class _NodeData:
 
             self.clear()
 
-            temp_container: list[dict[str, Any]] = []
-            count = 0
+            data: list[dict[str, bool | float | int | str]] = []
+            first_call = True
             while return_value.value < 2:
                 return_value.value = self._dll.get(
                     1,
@@ -122,24 +154,27 @@ class _NodeData:
                     0,
                     byref(node),
                     byref(record_length),
-                    0 if count == 0 else 1
+                    0 if first_call else 1
                 )
 
+                record_length = c_int(sizeof(node))
+                first_call = False
                 if return_value.value >= 2:
                     break
 
-                temp_container.append({
+                data.append({
                     "ID": node.m_nr,
-                    "INT_ID": node.m_inr,
                     "X0": node.m_xyz[0],
                     "Y0": node.m_xyz[1],
                     "Z0": node.m_xyz[2],
                     "KFIX": decode_nodal_boundary_condition(node.m_kfix),
-                    "NOT_USED": (node.m_ncod & 3) > 0
+                    "IS_USED": (node.m_ncod & 2) > 0
                 })
 
-                record_length = c_int(sizeof(node))
-                count += 1
-
-            self._data = DataFrame(temp_container)
+            self._data = DataFrame(data).set_index(["ID"], drop=False)
             self._is_loaded = True
+
+    def number_of_nodes(self) -> int:
+        """Return the number of nodes.
+        """
+        return self._data.ID.size
